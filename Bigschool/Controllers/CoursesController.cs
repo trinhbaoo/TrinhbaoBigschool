e16f15fbadedd5f19ff80736d632abd76f8d6f95
trinhbaoo <thtb11111@gmail.com>
2023-03-13 23:43:38 +0700

    Add project files.

---
+++ Bigschool/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using Bigschool.Models;
+using Bigschool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bigschool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                categories = _dbContext.Categories.ToList(),
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Bigschool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public Byte Category { get; set; }
+        public IEnumerable<Category> categories { get; set; }
+        public DateTime GetDateTime()
+        { return DateTime.Parse(string.Format("{ 0} { 1}", Date, Time)); }
+    }
+}
